46e48b6736b224cbce26c1392acd39fb060646a1
emilie-martin <emmy.mar@hotmail.com>
2022-05-12 15:40:04 -0400

    add visible links to TOC sections
    made title a link and added a "learn more" hyperlink too

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,4 +1,5 @@
 import useBaseUrl from "@docusaurus/useBaseUrl";
+import Translate from "@docusaurus/Translate";
 import React from "react";
 
 import "../css/Section.css";
@@ -12,11 +13,12 @@ const Section = ({index, title, description, link, keywords, image}) => {
       </div>
       <div className="section-details">
         <span className="section-index">{(index + 1) < 10 ? "0" + (index+1) : index}.</span>
-        <h3>{title}</h3>
+        <h3><a href={link}>{title}</a></h3>
         <p className="section-descr">{description}</p>
         <div className="section-keywords">
           {Object.values(keywords).map((item, index) => <span key={index}>{item}</span>)}
         </div>
+        <a href={link}><Translate>Learn more...</Translate></a>
       </div>
     </article>
   );
